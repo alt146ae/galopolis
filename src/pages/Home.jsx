5bedac44d9771be93c4312090b4d1b5d56219c85
alt146ae <emanuel.cortezm@gmail.com>
2026-06-03 21:30:45 -0600

    changenNav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/Navbar.jsx';
+import Navbar from '../components/NavBar';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import MenuSection from '../components/MenuSection';
