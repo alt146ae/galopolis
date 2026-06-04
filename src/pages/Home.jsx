86b8a325e6e113ee2d5ab63ba3db5563c0b0523f
alt146ae <emanuel.cortezm@gmail.com>
2026-06-03 21:12:06 -0600

    nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar.jsx';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import MenuSection from '../components/MenuSection';
